e20945a1b16384339ea59ae03361a9e87b7dc2d1
erzanovadilhan3-dotcom <erzanovadilhan3@gmail.com>
2026-01-31 03:04:06 +0500

    Add Practice2 Python basics examples

---
+++ python-basics/strings.py
@@ -0,0 +1,12 @@
+a ="Hello!"
+print(a[0:4])
+b="hel"
+c="lo"
+print(b+c)
+d="HaPpYnEwYeAr"
+print(d.lower())
+f="good moRNing"
+print(f.upper())
+subject="programmingprinciplesii"
+txt=f"Now I\'m practicing {subject}"
+print(txt)
